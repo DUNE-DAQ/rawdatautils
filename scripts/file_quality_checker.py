c0120405efd298215a165c053f0708c1cb35f7e5
John Christian Freeman <jofreema@np04-srv-001.cern.ch>
2022-11-17 13:31:39 +0100

    JCF: Issue #14: add summaries of info on sequence #'s, fragment types, counts and sizes

--- scripts/file_quality_checker.py
+++ scripts/file_quality_checker.py
@@ -30,15 +30,64 @@ def main(filename, nrecords):
         records_to_process = records[:nrecords]
     print(f'Will process {len(records_to_process)} of {len(records)} records.')
 
+    sequence_ids = [] # print only if a problem
+    tr_global_stats = {}
+    
     for r in records_to_process:
 
         print("")
         print(f'Processing (Record Number,Sequence Number)=({r[0],r[1]})')
         dset_paths = h5_file.get_fragment_dataset_paths(r)
-
+        sequence_ids.append(r[1])
+        tr_stats = {}
+        
         for dset_path in dset_paths:
             frag = h5_file.get_frag(dset_path)
             print(f"{frag.get_fragment_type()} {frag.get_trigger_number()} {frag.get_sequence_number()} {frag.get_size()}") #  {int(frag.get_error_bits())}
+            if frag.get_fragment_type() in tr_stats:
+                tr_stats[ frag.get_fragment_type()]["count" ] += 1
+                if frag.get_size() > tr_stats[ frag.get_fragment_type() ]["max_size" ]:
+                    tr_stats[ frag.get_fragment_type() ][ "max_size" ] = frag.get_size()
+                if frag.get_size() < tr_stats[ frag.get_fragment_type()]["min_size" ]:
+                    tr_stats[ frag.get_fragment_type()]["min_size" ] = frag.get_size()
+            else:
+                tr_stats[ frag.get_fragment_type() ] = { "count":1, "max_size": frag.get_size(), "min_size": frag.get_size() }
 
+        for frag_type in tr_stats:
+            if frag_type in tr_global_stats:
+                if tr_stats[frag_type]["count"] > tr_global_stats[frag_type]["max_count"]:
+                    tr_global_stats[frag_type]["max_count"] = tr_stats[frag_type]["count"]
+                if tr_stats[frag_type]["count"] < tr_global_stats[frag_type]["min_count"]:
+                    tr_global_stats[frag_type]["min_count"] = tr_stats[frag_type]["count"]
+                if tr_stats[frag_type]["max_size"] > tr_global_stats[frag_type]["max_size"]:
+                    tr_global_stats[frag_type]["max_size"] = tr_stats[frag_type]["max_size"]
+                if tr_stats[frag_type]["min_size"] < tr_global_stats[frag_type]["min_size"]:
+                    tr_global_stats[frag_type]["min_size"] = tr_stats[frag_type]["min_size"]
+
+            else:
+                tr_global_stats[frag_type] = { "max_count": tr_stats[frag_type]["count"],
+                                               "min_count": tr_stats[frag_type]["count"],
+                                               "max_size": tr_stats[frag_type]["max_size"],
+                                               "min_size": tr_stats[frag_type]["min_size"]
+                                               }
+
+
+    if len(set(sequence_ids)) == 1:
+        print(f"All sequence #'s are the same, {sequence_ids[0]}")
+    elif len(set(sequence_ids)) == len(sequence_ids) and \
+         max(sequence_ids) - min(sequence_ids) + 1 == len(sequence_ids):
+        print(f"All sequence #'s follow one another, counting from {min(sequence_ids)} to {max(sequence_ids)}")
+    else:
+        print("Sequence #'s are neither identical nor increasing one-by-one: ")
+        print(" ".join([str(seqid) for seqid in sequence_ids]))
+        print("")
+        
+    for frag_type in tr_global_stats:
+        print(f"{frag_type}: max # in a record == %s, min # in a record == %s, max size == %s bytes, min size == %s bytes" %
+              (tr_global_stats[frag_type]["max_count"], tr_global_stats[frag_type]["min_count"],
+               tr_global_stats[frag_type]["max_size"], tr_global_stats[frag_type]["min_size"]))
+
+    
+        
 if __name__ == '__main__':
     main()
