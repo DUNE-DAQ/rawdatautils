e4f11224be752b3718f91afbac6d7be480641f92
Kurt Biery <biery@daq.fnal.gov>
2025-04-17 09:30:06 -0500

    Updated the daphne_decoder script to be able to print out frame timestamps for streaming data as well as self-triggered data.

--- scripts/daphne_decoder.py
+++ scripts/daphne_decoder.py
@@ -65,10 +65,10 @@ def print_links(pds_geo_ids):
 @click.option('--det', default='HD_PDS', help='Subdetector string (default: HD_PDS)')
 @click.option('--nrecords', '-n', default=-1, help='How many Trigger Records to process (default: all)')
 @click.option('--nskip', default=0, help='How many Trigger Records to skip (default: 0)')
-@click.option('--summary', is_flag=True, help="Print checks summary")
-@click.option('--check_ts', is_flag=True, help="Print timestamps check")
-@click.option('--adc_stats', is_flag=True, help="Print adc stats")
-@click.option('--print_frame_timestamps', is_flag=True, help="Print individual frame timestamps (can be verbose)")
+@click.option('--summary', is_flag=True, help="Print checks summary (currently broken?)")
+@click.option('--check_ts', is_flag=True, help="Print timestamps check (works for streaming data)")
+@click.option('--adc_stats', is_flag=True, help="Print adc stats (works for streaming data)")
+@click.option('--print_frame_timestamps', is_flag=True, help="Print individual frame timestamps (can be very verbose)")
 
 def main(filename, det, nrecords, nskip, adc_stats, check_ts, summary, print_frame_timestamps):
 
@@ -133,7 +133,6 @@ def main(filename, det, nrecords, nskip, adc_stats, check_ts, summary, print_fra
         for gid in pds_geo_ids:
             
             frag     = h5_file.get_frag(r,gid)
-            #geo_info = detchannelmaps.HardwareMapService.parse_geo_id(gid)
             det_link = 0xffff & (gid >> 48);
             det_slot = 0xffff & (gid >> 32);
             det_crate = 0xffff & (gid >> 16);
@@ -150,13 +149,14 @@ def main(filename, det, nrecords, nskip, adc_stats, check_ts, summary, print_fra
                 channels   = np_array_channels(frag)
                 n_channels = len(np.unique(channels))
 
-            elif fragType == 13:
+            elif fragType == FragmentType.kDAPHNEStream.value:
 
                 first_frame = fddetdataformats.DAPHNEStreamFrame(frag.get_data())
                 timestamps = np_array_timestamp_stream(frag)
                 adcs       = np_array_adc_stream(frag)
                 channels   = np_array_channels_stream(frag)[0]
                 n_channels = len(np.unique(channels))
+                #print(f'Frame size = {first_frame.sizeof()}, number of timestamps, adcs, channels, channels_in_list = {len(timestamps)}, {len(adcs)}, {len(channels)}, {len(np_array_channels_stream(frag))}')
 
             trigger_stamps.append(frag.get_trigger_timestamp())
 
@@ -189,23 +189,31 @@ def main(filename, det, nrecords, nskip, adc_stats, check_ts, summary, print_fra
 
                 print(line)
 
-            if tslot == det_slot:
-                continue
-            else:
-                tslot = det_slot
-                print("")
-
             if (print_frame_timestamps):
+                temp_channels = np_array_channels_stream(frag)
                 temp_dashes_string = "-"*110
                 print(f"    {temp_dashes_string}")
                 print("      --> Frame timestamp details <--")
                 print("      Index  Channel  DTS Timestamp (ticks)  DTS Timestamp (time string)")
                 print(f"    {temp_dashes_string}")
+                loop_counter = 0;
                 for idx in range(len(timestamps)):
+                    if fragType == FragmentType.kDAPHNEStream.value and (idx % 64) != 0:
+                        continue
                     ts_nsec = float(timestamps[idx])/62500000.0
                     time_string = datetime.datetime.fromtimestamp(ts_nsec)
-                    print(f'     {idx:>5}   {channels[idx]:>5}    {timestamps[idx]:>20}    {str(time_string):<26}')
+                    if fragType == FragmentType.kDAPHNEStream.value:
+                        print(f'     {(idx/64):>5}   {temp_channels[loop_counter]}    {timestamps[idx]:>20}    {str(time_string):<26}')
+                    else:
+                        print(f'     {idx:>5}   {channels[idx]:>5}    {timestamps[idx]:>20}    {str(time_string):<26}')
+                    loop_counter += 1
                 print()
+
+            if tslot == det_slot:
+                continue
+            else:
+                tslot = det_slot
+                print("")
 
         print(f"Number of active/total channels \t-- {active_channels:>20}/{scanned_channels}\n")
 
