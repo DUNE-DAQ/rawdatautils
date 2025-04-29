672620362e53982e016826e984f390a0a83bf2e6
Wesley Robert Ketchum <wketchum@fnal.gov>
2025-04-29 14:17:53 +0200

    update tpdecoder for using updated unpacker and channel map

--- scripts/tpdecoder.py
+++ scripts/tpdecoder.py
@@ -5,6 +5,7 @@ import h5py
 
 import daqdataformats
 import trgdataformats
+import detchannelmaps
 
 import click
 import time
@@ -17,8 +18,9 @@ from rawdatautils.unpack.dataclasses import TriggerPrimitiveData
 @click.argument('filenames', nargs=-1, type=click.Path(exists=True))
 @click.option('--nrecords', '-n', default=-1, help='How many records to process (default: all)')
 @click.option('--nskip', default=0, help='How many records to skip (default: 0)')
+@click.option('--channel-map', default=None, help="Channel map to load (default: None)")
 
-def main(filenames, nrecords, nskip):
+def main(filenames, nrecords, nskip, channel_map):
 
     for filename in filenames:
 
@@ -43,6 +45,11 @@ def main(filenames, nrecords, nskip):
         print(f'Will process {len(records_to_process)} of {len(records)} records.')
 
         
+        #have channel numbers per geoid in here
+        ch_map = None
+        if channel_map is not None:
+            ch_map = detchannelmaps.make_map(channel_map)
+
         with h5py.File(h5_file.get_file_name(), 'r') as f:
             record_type = f.attrs["record_type"]
             print(f'Record type is {record_type}')
@@ -70,6 +77,12 @@ def main(filenames, nrecords, nskip):
                     if tp.version != trgdataformats.TriggerPrimitive.s_trigger_primitive_version:
                         sys.exit(f"ERROR: the TP data structure version found in the data ({tp.version}) does not match the version expected by this version of the software ({trgdataformats.TriggerPrimitive.s_trigger_primitive_version}). Please use a version of the software that matches the data.")
 
+                    plane=-1
+                    apa="Unknown"
+                    if ch_map is not None and (tp.detid==3 or tp.detid==10):
+                        plane=ch_map.get_plane_from_offline_channel(tp.channel)
+                        apa=ch_map.get_tpc_element_from_offline_channel(tp.channel)
+                        
                     tpd = TriggerPrimitiveData(run=frag.get_run_number(),
                                                trigger=frag.get_trigger_number(),
                                                sequence=frag.get_sequence_number(),
@@ -78,10 +91,13 @@ def main(filenames, nrecords, nskip):
                                                samples_to_peak=tp.samples_to_peak,
                                                samples_over_threshold=tp.samples_over_threshold,
                                                channel=tp.channel,
+                                               plane=plane,
+                                               apa=apa,
                                                adc_integral=tp.adc_integral,
                                                adc_peak=tp.adc_peak,
                                                detid=tp.detid,
-                                               flag=tp.flag)
+                                               flag=tp.flag,
+                                               id_ta=-1)
                     print(tpd)
                     
                 
